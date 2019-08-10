9d3e4922bd1ea9f08f9ad5733885d320604c34b5
Michał Salamon <ms055452@student.ath.edu.pl>
2019-08-10 13:36:08 +0200

    Operations added

---
+++ Calculator/Multiplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator.Calculator
+{
+    class Multiplication
+    {
+        private double x;
+
+        public double Wynik(double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Calculator/Substraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator.Calculator
+{
+    class Substraction
+    {
+        private double x;
+
+        public double Wynik(double y)
+        {
+            return x - y;
+        }
+    }
+}
